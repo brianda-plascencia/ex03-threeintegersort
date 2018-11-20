cec506d0be8f465144205bddfeb6dd8f137a5932
brianda-plascencia <brianda_plascencia1@my.vcccd.edu>
2018-11-20 09:59:18 -0800

    Turning in

---
+++ src/threeintegersort.h
@@ -0,0 +1,23 @@
+//
+// Created by vscilab on 11/20/2018.
+//
+
+#ifndef EX03_THREEINTEGERSORT_THREEINTEGERSORT_H
+#define EX03_THREEINTEGERSORT_THREEINTEGERSORT_H
+
+#ifndef LECTURE_SORT_H
+#define	LECTURE_SORT_H
+using namespace std;
+namespace edu{
+    namespace vcccd {
+        namespace vc {
+            namespace csv13 {
+                int sort(int &, int &, int &);
+
+#endif
+            }
+        }
+    }
+}
+
+#endif //EX03_THREEINTEGERSORT_THREEINTEGERSORT_H
